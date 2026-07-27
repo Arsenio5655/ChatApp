3e7922b34f2446e8fa27a2a86655047e98310e8d
hvenanc <hvs.git@gmail.com>
2026-07-27 19:59:47 -0300

    Chat App - Atividade Parcial

--- src/service/supabase.js
+++ src/service/supabase.js
@@ -2,10 +2,10 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://ttxhdkdthknfpfxjmlmz.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0eGhka2R0aGtuZnBmeGptbG16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxMDgyNzAsImV4cCI6MjA5ODY4NDI3MH0.8xLXh42pfsCqo57b7uKQk-geXC2U0Td9jGmR9h7vLX8';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? '';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? '';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
